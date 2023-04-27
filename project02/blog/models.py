0e0d1871e0a72c3a083cf00fc155fd9a0d3b9184
JUYOUNG <juju753@naver.com>
2023-04-27 22:27:18 +0900

    django_22

--- project02/blog/models.py
+++ project02/blog/models.py
@@ -2,6 +2,5 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('data published')
+    pub_date = models.DateTimeField('data publishedqq')
     body = models.TextField()
-    
